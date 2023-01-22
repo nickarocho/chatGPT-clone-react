e1b15bbd6b6c205fff38bfa7434fc445ae95334e
nickarocho <nick@boldlyfine.com>
2023-01-22 09:33:09 -0800

    generate an image with Dall-E 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
 import { Configuration, OpenAIApi } from "openai";
+import { useState } from "react";
+import dallEcat from "./assets/dallE-cat.png";
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const [generatedImageResultSrc, setGeneratedImageResultSrc] = useState<
+    string | undefined
+  >(undefined);
+
   const config = new Configuration({
     apiKey: import.meta.env.VITE_Open_AI_Key,
   });
@@ -9,19 +16,58 @@ function App() {
   const openai = new OpenAIApi(config);
 
   const generateImage = async () => {
-    const response = await openai.createImage({
-      prompt: "a very fat and fluffy cat wearing sunglasses on the beach",
-      n: 1,
-      size: "1024x1024",
-    });
+    if (!prompt) {
+      alert("Gotta add a prompt first...");
+      return;
+    }
 
-    console.log(response.data);
+    try {
+      const response = await openai.createImage({
+        prompt: prompt,
+        n: 1,
+        size: "1024x1024",
+      });
+
+      setGeneratedImageResultSrc(response.data.data[0].url);
+    } catch (error) {
+      console.error(
+        "Something went wrong generating the image with OpenAI:",
+        error
+      );
+    }
   };
 
   return (
     <div className="App">
       <h1>ChatGPT Clone</h1>
-      <button onClick={generateImage}>Do the thing</button>
+      <div className="app-main">
+        <h2>Generate an image with AI</h2>
+        <h4>
+          Just add a prompt for anything you can possibly think of (literally
+          anything...), and OpenAI's "DALL-E" will do the rest.
+        </h4>
+        <h3>Input 👇</h3>
+        <input
+          className="app-input"
+          type="text"
+          placeholder="A very fluffy cat wearing sunglasses on the beach, in the style of Salvador Dalí."
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+        <button onClick={generateImage}>Make AI Art</button>
+
+        <div className="output-container">
+          <h3>Output 👇</h3>
+          <img
+            className="result-image"
+            src={
+              !generatedImageResultSrc && !prompt
+                ? dallEcat
+                : generatedImageResultSrc
+            }
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 }
